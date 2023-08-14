56bc257701ab2f92506d9a69da7e75ade5c261d0
Big Cat <xnhyzhyh@gmail.com>
2023-08-14 21:45:18 +0800

    Update exec_epggrabber.sh

--- exec_epggrabber.sh
+++ exec_epggrabber.sh
@@ -3,4 +3,4 @@
 python3 -m pip install requests
 sudo apt-get install xmltv-util
 
-tv_grep --output ./epg.xml --channel-id 新唐人亚太台 REGEXP https://raw.githubusercontent.com/iptv-pro/iptv-pro.github.io/main/epg/epg.xml
+tv_grep --output ./epg.xml --channel-id 新唐人亚太台 REGEXP https://github.com/iptv-pro/iptv-pro.github.io/blob/main/epg/epg.xml
