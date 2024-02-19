26869b0249d95b28ca84c347772399f4b9bd0667
Big Cat <xnhyzhyh@gmail.com>
2024-02-19 23:10:15 +0800

    Update YouTubeLinkGrabber.py

--- YouTubeLinkGrabber.py
+++ YouTubeLinkGrabber.py
@@ -2,26 +2,25 @@
 import requests
 import os
 
-def grab(url, ch_name):
-    response = requests.get(url, timeout=15).text
+def grab(url):
+    response = requests.get(url, timeout = 15).text
     if '.m3u8' not in response:
-        with open(f'{ch_name}.m3u', 'w', encoding='utf-8') as file:
-            file.write(f"### {ch_name}\n")
+        print("###") #print("https://www.youtube.com/watch?v=1oh9IEwBbFY")
         return
     end = response.find('.m3u8') + 5
     tuner = 100
     while True:
-        if 'https://' in response[end - tuner: end]:
-            link = response[end - tuner: end]
+        if 'https://' in response[end - tuner : end]:
+            link = response[end - tuner : end]
             start = link.find('https://')
             end = link.find('.m3u8') + 5
             break
         else:
             tuner += 5
-    with open(f'{ch_name}.m3u', 'w', encoding='utf-8') as file:
-        file.write(f"{link[start: end]} {ch_name}\n")
+    print(f"{link[start : end]}")
 
 with open('./youtubeLink.txt', encoding='utf-8') as f:
+    print(f'#EXTM3U\n')
     for line in f:
         line = line.strip()
         if not line or line.startswith('##'):
@@ -31,11 +30,9 @@ with open('./youtubeLink.txt', encoding='utf-8') as f:
             ch_name = line[0].strip()
             grp_title = line[1].strip().title()
             ch_logo = line[2].strip()
-            #with open(f'{ch_name}.m3u', 'w', encoding='utf-8') as file:
-                #file.write('#EXTM3U\n')
-                #file.write(f'\n#EXTINF:-1 tvg-name="{ch_name}" group-title="{grp_title}" tvg-logo="{ch_logo}", {ch_name}\n')
+            print(f'\n#EXTINF:-1 tvg-name="{ch_name}" group-title="{grp_title}" tvg-logo="{ch_logo}", {ch_name}')
         else:
-            grab(line, ch_name)
+            grab(line)
             
 if 'temp.txt' in os.listdir():
     os.system('rm temp.txt')
